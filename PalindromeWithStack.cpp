298adf6053d36399c53ad9c80114579b4f24d35e
Abdallah Amir <abdallahamirmonir@gmail.com>
2023-02-25 18:49:51 +0200

    Update PalindromeWithStack.cpp

--- PalindromeWithStack.cpp
+++ PalindromeWithStack.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 using namespace std;
 
-//Doctor Nada this took me 6 hours my eyes are red and i'm hungry but it was worth it for the bonus I hope you're happy XD
-
 //Declaring the stack array
 const int aSize = 10;
 string stack[aSize];
